52cfea05aedba5d8daffb0ce4036a517674a0d88
roger.miranda <roger.miranda@students.salle.url.edu>
2021-07-17 01:40:50 +0200

    Secure functions

--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
-#include <cfenv>
 #include <iostream>
+#include "secure_math.h"
 
 int main() {
-    std::feclearexcept(FE_OVERFLOW);
-    std::feclearexcept(FE_UNDERFLOW);
-
-    double overflowing_var = 1000;
-    double underflowing_var = 0.01;
-
-    std::cout << "Overflow flag before: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
-    std::cout << "Underflow flag before: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
-
-    for(int i = 0; i < 20; ++i) {
-        overflowing_var *= overflowing_var;
-        underflowing_var *= underflowing_var;
-    }
-
-    std::cout << "Overflow flag after: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
-    std::cout << "Underflow flag after: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
+    bool error;
+    std::cout << secureFactorial(&error, 34.0) << " (" << error << ")" << std::endl;
     return 0;
 }
