538a14c4a7aa5a2c37dc8109c65050eedb7809df
roger.miranda <roger.miranda@students.salle.url.edu>
2021-07-16 23:17:06 +0200

    Check overflow/underflow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#include <cfenv>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::feclearexcept(FE_OVERFLOW);
+    std::feclearexcept(FE_UNDERFLOW);
+
+    double overflowing_var = 1000;
+    double underflowing_var = 0.01;
+
+    std::cout << "Overflow flag before: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
+    std::cout << "Underflow flag before: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
+
+    for(int i = 0; i < 20; ++i) {
+        overflowing_var *= overflowing_var;
+        underflowing_var *= underflowing_var;
+    }
+
+    std::cout << "Overflow flag after: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
+    std::cout << "Underflow flag after: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
     return 0;
 }
